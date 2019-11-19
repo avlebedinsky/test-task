ed38825dcc0fcc83b0ed9315e2caa222cb41d5b5
lebedinsky <avlebedinsky@gmail.com>
2019-11-19 23:46:57 +0300

    do footer

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,14 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function () {
+    // eslint-disable-next-line no-undef
+    ymaps.ready(init)
+
+    function init () {
+        // eslint-disable-next-line no-undef
+        const myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 7
+        })
+        console.log(myMap)
+    }
 })
